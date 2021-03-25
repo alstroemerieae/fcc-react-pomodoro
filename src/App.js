9c01d480b4f0b08e37cd0ae579529caa94c372c9
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-03-25 16:29:26 -0300

    Add content and basic layout

--- src/App.js
+++ src/App.js
@@ -2,7 +2,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="">
+    <div className="container">
+      <div className="pomodoro">
+        {/* Break control */}
+        <div className="length-control">
+          <div id="break-label">Break Length</div>
+          <div className="break-controls">
+            <div id="break-decrement">+</div>
+            <div id="break-length">5</div>
+            <div id="break-increment">-</div>
+          </div>
+        </div>
+        {/* Session control */}
+        <div className="length-control">
+          <div id="session-label">Session Length</div>
+          <div className="session-controls">
+            <div id="session-decrement">+</div>
+            <div id="session-length">25</div>
+            <div id="session-increment">-</div>
+          </div>
+        </div>
+        {/* Timer display */}
+        <div className="timer">
+          <div id="timer-label">Session</div>
+          <div id="time-left">25:00</div>
+          <div className="timer-controls">
+            <div id="start_stop">Stop</div>
+            <div id="reset">Reset</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
