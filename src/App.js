455237d87c57de3ae06c642e9b03c383c95df755
Luciano Alcaraz <LGA-dev@hotmail.com>
2021-03-29 16:08:56 -0300

    Bug fixes

--- src/App.js
+++ src/App.js
@@ -9,33 +9,69 @@ function App() {
   const [breakLength, setBreakLength] = useState(5);
   const [sessionLength, setSessionLength] = useState(25);
 
+  const [secondsDisplay, setSecondsDisplay] = useState("66")
+  const [minutesDisplay, setMinutesDisplay] = useState("77")
+
+
+  if(secondsDisplay === "0") {
+    console.log("AAA")
+    setSecondsDisplay("00");
+  } else if (secondsDisplay === "1") {
+    setSecondsDisplay("01");
+  } else if (secondsDisplay === "2") {
+    setSecondsDisplay("02");
+  } else if (secondsDisplay === "3") {
+    setSecondsDisplay("03");
+  } else if (secondsDisplay === "4") {
+    setSecondsDisplay("04");
+  } else if (secondsDisplay === "5") {
+    setSecondsDisplay("05");
+  } else if (secondsDisplay === "6") {
+    setSecondsDisplay("06");
+  } else if (secondsDisplay === "7") {
+    setSecondsDisplay("07");
+  } else if (secondsDisplay === "8") {
+    setSecondsDisplay("08");
+  } else if (secondsDisplay === "9") {
+    setSecondsDisplay("09");
+  }
+
   useEffect(() => {
+    // Set the minutes and seconds to string
+    setSecondsDisplay(seconds.toString());
+    setMinutesDisplay(minutes.toString());
+
     // Stop countdown if it reaches 00:00
-    // if (minutes === 0 && seconds === 0) {
-    //   console.log('Stopping countdown!');
-    //   setIsRunning(false);
-    // }
-    // Check if the app is set to run
+    if (minutes === 0 && seconds === 0) {
+      console.log('Stopping countdown!');
+      setIsRunning(false);
+    }
+
+    // Check if the app is set to run (isRunning === true)
     if (isRunning === true) {
-      // Set seconds to 59 if they are equal to 0 (after 1 second)
+      // Set seconds to (59) and minutes to (minutes-1) if seconds are equal to (0)
       if (seconds === 0) {
+        // Run this after 1 seconds
+        // This will prevent a display bug like this:
+        // 24:00 (1s)=> 23:00 (1s)=> 23:59
+        // And will display this instead:
+        // 24:00 (1s)=> 23:59
         const timer = setTimeout(() => {
           console.log('This will run after 1 second!')
-          setSeconds(59);
           setMinutes(minutes - 1);
-        }, 10);
-        // Clear setTimeout
+          setSeconds(59);
+        }, 1000);
         return () => clearTimeout(timer);
       }
-      // If app is set to run, execute the setInterval method
+      // Execute the setInterval method to substract 1 to seconds every second
       const interval = setInterval(() => {
         console.log('This will run every second!');
         setSeconds(seconds => seconds - 1);
-      }, 10);
+      }, 1000);
       // Clear setInterval
       return () => clearInterval(interval);
     }
-    // Check if the app is set to not run
+    // Check if the app is set to not run (isRunning === false)
     else {
       console.log('This will NOT run every second!');
     }
@@ -46,6 +82,10 @@ function App() {
     if (isRunning === false) {
       console.log("Start countdown");
       setIsRunning(true);
+      if (seconds === 0) {
+        setSeconds(59);
+        setMinutes(minutes - 1);
+      }
     }
     // Stop timer
     else if (isRunning === true) {
@@ -58,50 +98,53 @@ function App() {
     console.log("Restart countdown");
     setSeconds(0);
     setMinutes(25);
-    // Check what another pomodoros do when the reset button is clicked
-    // Do they restart the countdown on click (true), or the countdown should start after clicking Start again (false or remove setIsRunning)?
-    // Checked: delete this..
-    setIsRunning(true);
+    setBreakLength(5);
+    setSessionLength(25);
+    setIsRunning(false);
   }
 
   const decrementBreak = () => {
     console.log("Decrement break");
-    setBreakLength(breakLength - 1);
+    if (breakLength <= 0) {
+      console.log("Break has reached the minimum length (0)")
+      setBreakLength(1);
+    } else {
+      console.log(`Break length is ${breakLength}, substracting 1 from it...`)
+      setBreakLength(breakLength - 1);
+    }
   }
 
   const incrementBreak = () => {
     console.log("Increment break");
-    setBreakLength(breakLength + 1);
+    if (breakLength > 60) {
+      console.log("Break has reached the maximum length (60)")
+        return;
+    } else {
+      console.log(`Break length is ${breakLength}, adding 1 to it...`)
+      setBreakLength(breakLength + 1);
+    }
   }
 
   const decrementSession = () => {
     console.log("Decrement session");
-    setSessionLength(sessionLength - 1);
+    if (sessionLength <= 0) {
+      console.log("Session has reached the minimum length (0)")
+        return;
+    } else {
+      console.log(`Session length is ${sessionLength}, substracting 1 from it...`)
+      setSessionLength(sessionLength - 1);
+    }
   }
 
   const incrementSession = () => {
     console.log("Increment session");
-    setSessionLength(sessionLength + 1);
-  }
-
-  if (sessionLength <= 0) {
-    console.log("Session has reached the minimum length (0)")
-    setSessionLength(1);
-  }
-
-  if (breakLength <= 0) {
-    console.log("Break has reached the minimum length (0)")
-    setBreakLength(1);
-  }
-
-  if (sessionLength > 60) {
-    console.log("Session has reached the maximum length (60)")
-    setSessionLength(60);
-  }
-
-  if (breakLength > 60) {
-    console.log("Break has reached the maximum length (60)")
-    setBreakLength(60);
+    if (sessionLength > 60) {
+      console.log("Session has reached the maximum length (60)")
+      setSessionLength(60);
+    } else {
+      console.log(`Session length is ${sessionLength}, adding 1 to it...`)
+      setSessionLength(sessionLength + 1);
+    }
   }
 
   return (
@@ -129,9 +172,9 @@ function App() {
         <div className="timer">
           <div id="timer-label">Session</div>
           <div id="time-left">
-            <div id="minutes">{ minutes }</div>
+            <div id="minutes">{ minutesDisplay }</div>
             <div id="divider">:</div>
-            <div id="seconds">{ seconds }</div>
+            <div id="seconds">{ secondsDisplay }</div>
           </div>
           <div className="timer-controls">
             <div id="start_stop" onClick={ startStopTimer }>Start/Stop</div>
